49db5b2cd9ba76b47794d2580a674ddda93e6b27
Tom Coleman <tom@percolatestudio.com>
2019-01-16 13:43:00 +1100

    Stories to test various things

--- src/stories/index.js
+++ src/stories/index.js
@@ -3,17 +3,40 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { isChromatic } from 'storybook-chromatic';
 
 import { Button, Welcome } from '@storybook/react/demo';
+
+URL.createObjectURL('foo', 'bar');
+URL.revokeObjectURL('foo', 'bar');
+new MutationObserver(() => 0);
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+  .add(
+    'with text',
+    () => (
+      <Button style={{ position: 'fixed', top: '100px' }} onClick={action('clicked')}>
+        Hello Button {isChromatic() ? 'CHROMATIC 🙆‍' : 'NOT CHROMATIC 🙍🏻‍♀'}
+      </Button>
+    ),
+    {
+      chromatic: { noScroll: true },
+    }
+  )
+  .add(
+    'with some emoji',
+    () => (
+      <Button onClick={action('clicked')}>
+        <span role="img" aria-label="so cool">
+          😀 😎 👍 💯
+        </span>
+      </Button>
+    ),
+    {
+      chromatic: {
+        disable: true,
+      },
+    }
+  );
